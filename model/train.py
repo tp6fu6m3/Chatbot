2d87bd20e8ea609cd206b540082627746a7c2b80
tp6fu6m3 <m3tp6fu6@gmail.com>
2021-01-03 17:53:36 +0800

    Add files via upload

--- model/train.py
+++ model/train.py
@@ -1,14 +1,51 @@
 # -*- coding: utf-8 -*-
-import logging
 from gensim.models import word2vec
+from gensim.models.callbacks import CallbackAny2Vec
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', type=str, default='wiki_seg.txt', help='input')
 args = parser.parse_args()
 
+class callback(CallbackAny2Vec):
+    def __init__(self):
+        self.epoch = 1
+        self.losses = []
+        self.cumu_loss = 0.0
+        self.previous_epoch_time = time.time()
+        self.best_model = None
+        self.best_loss = 1e+30
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        norms = [np.linalg.norm(v) for v in model.wv.vectors]
+        now = time.time()
+        epoch_seconds = now - self.previous_epoch_time
+        self.previous_epoch_time = now
+        self.cumu_loss += float(loss)
+        print(f'Loss after epoch {self.epoch}: {loss} (cumulative loss so far: {self.cumu_loss}) '+\
+              f'-> epoch took {round(epoch_seconds, 2)} s - vector norms min/avg/max: '+\
+              f'{round(float(min(norms)), 2)}, {round(float(sum(norms)/len(norms)), 2)}, {round(float(max(norms)), 2)}')
+        self.epoch += 1
+        self.losses.append(float(loss))
+        model.running_training_loss = 0.0
+        if loss < self.best_loss:
+            self.best_model = copy.deepcopy(model)
+            self.best_loss = loss
+        self.plot('loss.png')
+
+    def plot(self, path):
+        fig, (ax1) = plt.subplots(ncols=1, figsize=(6, 6))
+        ax1.plot(self.losses)
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend()
+        plt.savefig(path)
+        plt.close()
+
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     sentences = word2vec.LineSentence(args.input)
-    model = word2vec.Word2Vec(sentences, size=250)
+    model = word2vec.Word2Vec(sentences, size=100, min_count=1, 
+        window=5, compute_loss=True, callbacks=[callback()])
     model.save('word2vec.model')
